9cf1f279e419faa7669ce68a19e790b809fc3989
thilina <thilina.s.abeysinghe@gmail.com>
2016-11-13 23:29:56 +0530

    Second Commit

--- googleurl.js
+++ googleurl.js
@@ -1,1 +1,34 @@
 //coding to retrive google search
+var google = require('google');
+var events = require('events');
+
+google.resultsPerPage = 25
+var nextCounter = 0
+
+var result_ready = new events.EventEmitter();
+
+//store links in a variable
+var storeLinks = []
+
+var googleurls = function(keywords){
+
+var result = google(keywords, function(err, res){
+        if(err) throw err;
+
+        //store every links in storeLinks array
+        for(var i=0; i<res.links.length; i++){
+            storeLinks.push(res.href);
+            if(i==google.resultsPerPage){
+                result_ready.emit("fullyLoaded");
+            }
+        }
+
+        
+    });
+
+    result_ready.emit('fullyLoaded', function(result){
+        console.log(result);
+    });
+
+
+};
